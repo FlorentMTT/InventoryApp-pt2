1bb0f0b8efd8f7391ae7465a1080cfb0a55de0e2
FlorentMTT <florent.mttt@eduge.ch>
2023-03-18 13:14:51 +0100

    correction de bug (manquait appel pour l'enevement edit)

--- app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -63,6 +63,7 @@ class ItemDetailFragment : Fragment() {
             sellItem.isEnabled = viewModel.isStockAvailable(item)
             sellItem.setOnClickListener { viewModel.sellItem(item) }
             deleteItem.setOnClickListener { showConfirmationDialog() }
+            editItem.setOnClickListener { editItem() }
         }
     }
 
